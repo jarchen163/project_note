6541ec4e59dc60ae38c07774407dc2b97c46c495
chenyulong <jar_chen@163.com>
2020-10-31 18:01:27 +0800

    ds

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,13 +1,20 @@
 package com.example.springboot;
 
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+		   SpringApplication bootstrap = new SpringApplication(SpringbootApplication.class);
+		   bootstrap.setBanner(new ResourceBanner(new ClassPathResource("banner.txt")));
+	        bootstrap.setBannerMode(Banner.Mode.CONSOLE);
+	        // 其他定制设置...
+	        bootstrap.run(args);	}
 
 }
